9c3763e67104d967f3d83e4b389ed621fa77b0cf
DBouillette <david.bouillette@gmail.com>
2016-08-18 17:24:11 +0200

    V1

--- src/com/vo/vue/Fenetre.java
+++ src/com/vo/vue/Fenetre.java
@@ -1,11 +1,17 @@
 package com.vo.vue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import com.vo.model.LectureFichierXml;
 
 public class Fenetre extends JFrame {
 	
+	private JMenuBar menuPrincipal = null;
+	private JMenu fichier = null;
+	private JMenuItem quitter = new JMenuItem("quitter");
 	
 	// Initialisation de la fenetre principale
 	public Fenetre(LectureFichierXml config){
@@ -14,7 +20,17 @@ public class Fenetre extends JFrame {
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setContentPane(new PanelFond());
-		this.repaint();
+		
+	//Création de la barre de menu principal
+		menuPrincipal = new JMenuBar();
+	//Création des boutons du menu principal
+		quitter = new JMenuItem("Quitter");
+		fichier = new JMenu("Fichier");
+		fichier.add(quitter);
+		menuPrincipal.add(fichier);
+		this.setJMenuBar(menuPrincipal);
+			
+				
 		this.setVisible(true);
 		
 	}
