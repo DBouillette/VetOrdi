c568adb28c2ce4c381645173c151b334a5f644cd
DBouillette <david.bouillette@gmail.com>
2016-08-18 18:51:53 +0200

    V1 18082016

--- src/com/vo/vue/Fenetre.java
+++ src/com/vo/vue/Fenetre.java
@@ -1,5 +1,8 @@
 package com.vo.vue;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -11,9 +14,10 @@ public class Fenetre extends JFrame {
 	
 	private JMenuBar menuPrincipal = null;
 	private JMenu fichier = null;
-	private JMenuItem quitter = new JMenuItem("quitter");
+	private JMenuItem quitter = null;
+	private JMenuItem client = null;
 	
-	// Initialisation de la fenetre principale
+  //-- Initialisation de la fenetre principale ---------------------------------------
 	public Fenetre(LectureFichierXml config){
 		this.setTitle(config.LectureCleXMml("NomApplication"));
 		this.setSize(Integer.valueOf(config.LectureCleXMml("ResoX")), Integer.valueOf(config.LectureCleXMml("ResoY")));
@@ -21,13 +25,34 @@ public class Fenetre extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setContentPane(new PanelFond());
 		
-	//Création de la barre de menu principal
+		//-- Création de la barre de menu principal ----------------------------------------
 		menuPrincipal = new JMenuBar();
-	//Création des boutons du menu principal
-		quitter = new JMenuItem("Quitter");
+		
+		//-- Création du bouton Quitter du menu principal ---------------------------------
 		fichier = new JMenu("Fichier");
+		
+		//-- Création du bouton Quitter du menu principal ----------------------------------
+		quitter = new JMenuItem("Quitter");
+		
+		//-- Ajout du listenner sur le bouton quitter ------------------------------------
+		quitter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);	
+			}
+		});
+		
+		//-- Création du bouton Clients du menu principal ---------------------------------
+		client = new JMenuItem("Clients");
+		
+		fichier.add(client);
 		fichier.add(quitter);
+		
 		menuPrincipal.add(fichier);
+		
+		
+		
+		
 		this.setJMenuBar(menuPrincipal);
 			
 				
